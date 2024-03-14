67843ba45385a989214443d46de20e0d1fded33d
Ramdeni Akshith <117385626+ramdeniakki@users.noreply.github.com>
2024-03-14 18:51:48 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// Importing the necessary modules: fs for file system operations, http for creating HTTP server, and path for handling file paths.
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
+
+// Reading the content of the file './txt/input.txt' synchronously using 'readFileSync' function from 'fs' module.
+// 'utf-8' specifies the encoding of the file, which is necessary to correctly interpret special characters.
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+
+// Printing the content of the file to the console.
+console.log(textIn);
+
+// Creating a new string variable 'textout' containing the text "Hello ".
+const textout = `Hello `;
+
+// Writing the content of 'textout' variable to a new file './txt/output.txt' synchronously using 'writeFileSync' function from 'fs' module.
+fs.writeFileSync('./txt/output.txt', textout);
+
+// Printing a message to the console indicating that the file has been written successfully.
+console.log("File written");
+
+// Reading the content of the file './txt/start.txt' asynchronously using 'readFile' function from 'fs' module.
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+    // Handling errors if any.
+    if (err) {
+        console.error(err);
+        return;
+    }
+    // Logging the data read from the file.
+    console.log(data);
+});
+
+// Creating an HTTP server using the 'createServer' method from the 'http' module.
+const server = http.createServer((req, res) => {
+    // Extracting the requested URL path from the request object.
+    const pathName = req.url;
+
+    // Handling different routes based on the requested URL path.
+    if (pathName === '/' || pathName === '/overview') {
+        // Sending response for the '/' and '/overview' routes.
+        res.end('This is overview');
+    } else if (pathName === '/products') {
+        // Sending response for the '/products' route.
+        res.end('This is products');
+    } else {
+        // Sending response for any other route.
+        res.write('Page not Found');
+        res.end();
+        console.log(pathName)
+    }
+});
+
+// Making the server listen on port 8000 and localhost (127.0.0.1).
+server.listen(8000, '127.0.0.1', () => {
+    console.log("Listening to requests on port 8000");
+});
